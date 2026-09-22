f0a4d2978acbc4e3da895f97c25a0977de0e5c75
CowenZ <cowen_zuo@163.com>
2026-09-22 09:25:41 +0800

    chore: 版本号升到 0.1.1-alpha2
    1. 定版 0.1.1-alpha2：根 package.json、桌面包、README 与欢迎页的版本串一起更新。
    
    2. 打 tag v0.1.1-alpha2，随分支一并推送。

--- packages/desktop/src/renderer/src/pages/Welcome.tsx
+++ packages/desktop/src/renderer/src/pages/Welcome.tsx
@@ -8,7 +8,7 @@ import { BrandDocGlyph } from '../components/icons'
 import { CreateProjectWizard } from './CreateProjectWizard'
 import './welcome.css'
 
-const FALLBACK_VERSION = '0.1.1-alpha1'
+const FALLBACK_VERSION = '0.1.1-alpha2'
 
 interface RecentItem {
   path: string
